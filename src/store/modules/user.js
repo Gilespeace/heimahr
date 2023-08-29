0911b24ba3d118d3523554c292e5ab51772bd06f
Gilespeace <114575861+Gilespeace@users.noreply.github.com>
2023-08-29 09:46:26 +0800

    vuex结合js-cookies持久化存储

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,97 +1,26 @@
-import { login, logout, getInfo } from '@/api/user'
 import { getToken, setToken, removeToken } from '@/utils/auth'
-import { resetRouter } from '@/router'
-
-const getDefaultState = () => {
-  return {
-    token: getToken(),
-    name: '',
-    avatar: ''
-  }
-}
-
-const state = getDefaultState()
-
-const mutations = {
-  RESET_STATE: (state) => {
-    Object.assign(state, getDefaultState())
-  },
-  SET_TOKEN: (state, token) => {
-    state.token = token
-  },
-  SET_NAME: (state, name) => {
-    state.name = name
-  },
-  SET_AVATAR: (state, avatar) => {
-    state.avatar = avatar
-  }
-}
-
-const actions = {
-  // user login
-  login({ commit }, userInfo) {
-    const { username, password } = userInfo
-    return new Promise((resolve, reject) => {
-      login({ username: username.trim(), password: password }).then(response => {
-        const { data } = response
-        commit('SET_TOKEN', data.token)
-        setToken(data.token)
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
-  },
-
-  // get user info
-  getInfo({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      getInfo(state.token).then(response => {
-        const { data } = response
-
-        if (!data) {
-          return reject('Verification failed, please Login again.')
-        }
-
-        const { name, avatar } = data
-
-        commit('SET_NAME', name)
-        commit('SET_AVATAR', avatar)
-        resolve(data)
-      }).catch(error => {
-        reject(error)
-      })
-    })
-  },
-
-  // user logout
-  logout({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      logout(state.token).then(() => {
-        removeToken() // must remove  token  first
-        resetRouter()
-        commit('RESET_STATE')
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
-  },
-
-  // remove token
-  resetToken({ commit }) {
-    return new Promise(resolve => {
-      removeToken() // must remove  token  first
-      commit('RESET_STATE')
-      resolve()
-    })
-  }
-}
 
 export default {
-  namespaced: true,
-  state,
-  mutations,
-  actions
+  namespace: 'user', // 开启命名空间
+  state: {
+    token: getToken()// 从缓存(cookies)中读取初始值
+  },
+  mutations: {
+    setToken: (state, token) => {
+      state.token = token
+      setToken(token)// 同步到缓存
+    },
+    removeToken: (state) => {
+      state.token = null
+      removeToken()// 删除Vuex的token
+    }
+  },
+  actions: {
+    async login(context, data) {
+      // todo: 调用登录接口
+      const token = await login(data)
+      // 返回一个token 123456
+      context.commit('setToken', token)
+    }
+  }
 }
-
